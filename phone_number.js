c3377f574c1d3d3e05162425f2aecf1f4598c578
Palwol <mirim920@naver.com>
2021-09-01 21:39:44 +0900

    끝 4자리 빼고 별

---
+++ phone_number.js
@@ -0,0 +1,12 @@
+function solution(phone_number) {
+    const first_char = phone_number.slice(0,phone_number.length-4);
+    let stars = "";
+    for(let i=0; i<first_char.length; i++){
+        stars = stars+"*";
+    }
+    const number = phone_number.slice(phone_number.length-4,phone_number.length);
+    const answer = stars + number;
+    return answer;
+}
+
+console.log(solution("1234567"));
