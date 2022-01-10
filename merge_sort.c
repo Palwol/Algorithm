110662c497813d731fe3673587227f7e52250e79
Palwol <mirim920@naver.com>
2022-01-10 23:22:40 +0900

    merge sort

---
+++ merge_sort.c
@@ -0,0 +1,60 @@
+# include <stdio.h>
+# define MAX_SIZE 8
+int sorted[MAX_SIZE];
+
+void merge(int list[], int left, int mid, int right){
+  int i, j, k, l;
+  i = left;
+  j = mid+1;
+  k = left;
+
+  /* 분할 정렬된 list의 합병 */
+  while(i<=mid && j<=right){
+    if(list[i]<=list[j])
+      sorted[k++] = list[i++];
+    else
+      sorted[k++] = list[j++];
+  }
+
+  // 남아 있는 값들을 일괄 복사
+  if(i>mid){
+    for(l=j; l<=right; l++)
+      sorted[k++] = list[l];
+  }
+  // 남아 있는 값들을 일괄 복사
+  else{
+    for(l=i; l<=mid; l++)
+      sorted[k++] = list[l];
+  }
+
+  // 배열 sorted[](임시 배열)의 리스트를 배열 list[]로 재복사
+  for(l=left; l<=right; l++){
+    list[l] = sorted[l];
+  }
+}
+
+// 합병 정렬
+void merge_sort(int list[], int left, int right){
+  int mid;
+
+  if(left<right){
+    mid = (left+right)/2; // 중간 위치를 계산하여 리스트를 균등 분할 -분할(Divide)
+    merge_sort(list, left, mid); // 앞쪽 부분 리스트 정렬 -정복(Conquer)
+    merge_sort(list, mid+1, right); // 뒤쪽 부분 리스트 정렬 -정복(Conquer)
+    merge(list, left, mid, right); // 정렬된 2개의 부분 배열을 합병하는 과정 -결합(Combine)
+  }
+}
+
+void main(){
+  int i;
+  int n = MAX_SIZE;
+  int list[8] = {21, 10, 12, 20, 25, 13, 15, 22};
+
+  // 합병 정렬 수행(left: 배열의 시작 = 0, right: 배열의 끝 = 7)
+  merge_sort(list, 0, n-1);
+
+  // 정렬 결과 출력
+  for(i=0; i<n; i++){
+    printf("%d\n", list[i]);
+  }
+}
