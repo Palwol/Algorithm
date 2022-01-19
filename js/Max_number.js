90abb9e76148372d0fd1304bb0fe4f3aabfd67e7
Palwol <mirim920@naver.com>
2022-01-19 23:18:55 +0900

    max_number

--- js/Max_number.js
+++ js/Max_number.js
@@ -1,4 +1,6 @@
 function solution(numbers) {
-    var answer = '';
-    return answer;
+    const answer = numbers.map(number => String(number)).sort((a,b) => (b+a)-(a+b)).join('');
+    return answer[0] ==='0' ? '0' : answer;
 }
+
+console.log(solution([0,0,0,0]));
